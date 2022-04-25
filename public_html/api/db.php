3e2a4f45dbb00fa525479bbf6043d787b649ea32
AdminConfigAppCafeteria <102762725+AdminConfigAppCafeteria@users.noreply.github.com>
2022-04-24 17:34:14 -0700

    edicion de db.php por la nueva conexion

--- public_html/api/db.php
+++ public_html/api/db.php
@@ -1,7 +1,7 @@
 <?php
 
-$username = 'root';
-$password = '';
-$connection = new PDO( 'mysql:host=localhost;dbname=cafeteria_prueba', $username, $password );
+$username = 'appcafet_rootAdm';
+$password = 'acceso@rootbase';
+$connection = new PDO( 'mysql:host=192.185.131.114;dbname=cafeteria_prueba', $username, $password );
 
 ?>
