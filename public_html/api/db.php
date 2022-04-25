7841cc8f2521fb5b6048cd2eef13db1a0caafd7b
AdminConfigAppCafeteria <102762725+AdminConfigAppCafeteria@users.noreply.github.com>
2022-04-24 17:35:17 -0700

    edicion de nombre de base de datos

--- public_html/api/db.php
+++ public_html/api/db.php
@@ -2,6 +2,6 @@
 
 $username = 'appcafet_rootAdm';
 $password = 'acceso@rootbase';
-$connection = new PDO( 'mysql:host=192.185.131.114;dbname=cafeteria_prueba', $username, $password );
+$connection = new PDO( 'mysql:host=192.185.131.114;dbname=appcafet_cafeteria_prueba', $username, $password );
 
 ?>
